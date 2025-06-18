87a118fa379b38eaf471e1507181e53689d9429f
adityamatAI <aditya265matai@gmail.com>
2025-06-18 15:19:23 +0400

    bla

--- ai_blog_app/settings.py
+++ ai_blog_app/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
-
-
+from dotenv import load_dotenv
+load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'YourSecretKey'
+SECRET_KEY = os.getenv("secret_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
